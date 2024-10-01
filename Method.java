4289eb98dc061147ef01e9ebe50c0f04854ff9fe
PaviPatil2012 <pavitra9999999999@gmail.com>
2024-10-01 16:20:43 +0530

    1st time code

---
+++ Method.java
@@ -0,0 +1,11 @@
+public class Method{
+     public static void main (String [] args){
+         sub(10,5);
+         sub(20,5);
+         
+     }
+      public static void sub(int number1,int number2){
+      	int substractionvalue=number1-number2;
+          System.out.println(substractionvalue);
+      }
+}         
